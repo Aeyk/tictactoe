ece68d3e89427d3288dfb27ba3623233ad5a2777
Malik Kennedy <mksybr@gmail.com>
2019-01-02 15:02:15 -0500

    Move Grid specific functions into Grid Module

--- js/application.js
+++ js/application.js
@@ -1,86 +1,140 @@
+function indexOfEvery(array, value) {
+	var indexes = [], i;
+	for(i = 0; i < array.length; ++i) {
+		if(array[i] == value) {
+		  indexes.push(i);
+		}
+	}
+	return indexes || -1;
+}
+
 ;(function(){
   'use strict';
+	var grid = function () {
+    var _grid = [0, 0, 0,
+			           0, 0, 0,
+			           0, 0, 0];
+    var init = function() {};
+  
+    var _winners = [[0, 1, 2], [3, 4, 5], [6, 7, 8],
+		                [0, 3, 6], [1, 4, 7], [2, 5, 8],
+		                [0, 4, 8], [2, 4, 6]];
+    
+    var _xsTurn = false;
+    var _gameOver = false;
+    
+    var state = function() {
+      return {..._grid};
+    };
+
+    var nextTurn = function () {
+      _xsTurn = !_xsTurn;
+      return _xsTurn;
+    }
+    var currentTurn = function() {
+      if(_xsTurn)
+        return "X";
+      else if(!_xsTurn)
+        return "O";
+    }
+    var isItemEmpty = function(xy) {
+      if(_grid[xy] === 0) 
+        return true;
+      else
+        return false;
+    }
+    var putMove = function(xy) {
+      if(isItemEmpty(xy)) {
+        _grid[xy] = !_xsTurn ? "X" : "O";
+        nextTurn();
+      }
+      return _grid[xy];
+    }
+
+    var isWinner = function() {
+      var xresult = [];
+      _winners
+        .forEach(
+          row => xresult.push(compareArrays(row,
+                                            indexOfEvery(_grid,
+                                                         "X"))));
+      var oresult = [];
+      _winners
+        .forEach(
+          row => oresult.push(compareArrays(row,
+                                            indexOfEvery(_grid,
+                                                         "O"))));
+      if(xresult.some(r => r === true))
+        return "X";
+      if(oresult.some(r => r === true))
+        return "O";
+      else
+        return false;
+    }
+
+    var isGridFull = function() {
+      return !(_grid.some(g => g === 0));
+    }
+
+    var isGameOver = function() {
+      var winningState = isWinner();
+      if(_gameOver)
+        return true;
+      if(isGridFull())
+        return true;
+      if(isWinner())
+        return winningState;
+      
+    }
+    
+    return {
+      state: state,
+      init: init,
+      nextTurn: nextTurn,
+      currentTurn: currentTurn,
+      isItemEmpty: isItemEmpty,
+      putMove: putMove,
+      isWinner: isWinner,
+      isGridFull: isGridFull,
+      isGameOver: isGameOver
+    }
+  }();
+
+	function compareArrays(a1, a2) {
+	  return $(a1).not(a2).length === 0 &&
+		  $(a2).not(a1).length === 0;
+	}
+	var winners = [[0, 1, 2], [3, 4, 5], [6, 7, 8],
+		             [0, 3, 6], [1, 4, 7], [2, 5, 8],
+		             [0, 4, 8], [2, 4, 6]];
+  var xsTurn = true;
+  var winnersResults = [];
+	var winningLog = "";
+	var gameOver = false;
 
   $(document).ready(function() {
-	  var playingGrid = [0, 0, 0,
+    var expGrid = grid;
+    var playingGrid = [0, 0, 0,
 			                 0, 0, 0,
-			                 0, 0, 0];
-	  var winners = [[0, 1, 2], [3, 4, 5], [6, 7, 8],
-		               [0, 3, 6], [1, 4, 7], [2, 5, 8],
-		               [0, 4, 8], [2, 4, 6]];
-
-	  /*
-	    0 0 0  a b c 
-	    0 0 0  d e f
-	    0 0 0  g h i
-
-	  */
-	  var playingGridDom = $(".tictactoe");
-	  var xsTurn = true;
-
-	  function compareArrays(a1, a2) {
-	    return $(a1).not(a2).length === 0 &&
-		    $(a2).not(a1).length === 0;
-	  }
-
-	  var winnersResults = [];
-	  var winningLog = "";
-	  var gameOver = false;
-
+			                 0, 0, 0];	  
 	  $(".box").click(function() {
-	    if(gameOver)
-		    return 0;
-	    if(xsTurn && $(this).text().includes("_")) {
-		    playingGrid[$(this).index()] = 1 || 0;
-		    $(this).text("X");
-		    xsTurn = !xsTurn;
-		    winners
-		      .forEach(winningRow =>
-			             winnersResults.push(
-				             compareArrays(winningRow,
-					                         indexOfEvery(playingGrid,
-							                                  1))));
-		    gameOver = winnersResults.some(r => r === true);
-		    if(gameOver)
-		      var winningLog = "X has Won!";		
-	    }
-	    else if (!xsTurn && $(this).text().includes("_")) {
-		    playingGrid[$(this).index()] = 2 || 0;
-		    $(this).text("O");
-		    xsTurn = !xsTurn;
-		    winners
-		      .forEach(winningRow =>
-			             winnersResults.push(
-				             compareArrays(winningRow,
-					                         indexOfEvery(playingGrid,
-							                                  2))));
-		    gameOver = winnersResults.some(r => r === true);
-		    if(gameOver) 
-		      var winningLog = "O has Won!";
-		  }
-	    
-		  if(gameOver) {
-		    $("h1").text(winningLog);
-		  } else if(isGridFull(playingGrid)) {
-		    winningLog = "Cats Game!"
-		    $("h1").text(winningLog);
-		  }
-	    
+      if(expGrid.isGameOver()) 
+        return 0; 
+      
+      var xy = $(this).index();
+      
+      if(expGrid.isItemEmpty(xy)) {
+        expGrid.putMove(xy);
+      }
+      
+      $(this).text(expGrid.currentTurn());
+      
+      console.log(expGrid.state());
 	  });
 
 	  $(".reload").click(function() {
 	    location.reload();
 	  });
-
-	  function indexOfEvery(array, value) {
-	    var indexes = [], i;
-	    for(i = 0; i < array.length; ++i) {
-		    if(array[i] == value) {
-		      indexes.push(i);
-		    }
-	    }
-	    return indexes || -1;
-	  }
 	  
 	  function isGridFull(grid) {
 	    for(var i = 0; i < grid.length; ++i) {
